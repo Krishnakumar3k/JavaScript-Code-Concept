261d4cde151d9207517964e044aec5ee653a0792
Krishnakumar3k <krishnakumar3kgt@gmail.com>
2023-09-29 00:27:18 +0530

    intialcommit

--- 3 - Functions and parameter/function.js
+++ 3 - Functions and parameter/function.js
@@ -9,7 +9,27 @@
 // }
 // myName();
   
-function addTwoNum(num1, num2){
-    console.log(num1+num2)
+// function addTwoNum(num1, num2){
+//     console.log(num1+num2)
+// }
+// addTwoNum(2,5);
+
+/* function addTwoNum(num1, num2){
+  let result = num1+num2
+  return result;
 }
 addTwoNum(2,5);
+ */
+
+// Function with return
+/* function loginUser(userName){
+    return `${userName} logged in`
+}
+console.log(loginUser("Krishna")); //Krishna logged in
+console.log(loginUser()); //undefined logged in
+ */
+
+/* function calculateCartPrice(...num1){
+  return num1;
+}
+console.log(calculateCartPrice(200, 400, 500)); */
---
+++ 3 - Functions and parameter/scope.js
@@ -0,0 +1,22 @@
+
+// if(true){
+//     let a = 10;
+//     const b = 20;
+//     var c = 30;
+// }
+
+
+// console.log(a);
+// console.log(b);
+//  console.log(c);
+
+function one(){
+    const userName = "Krishna";
+    function two(){
+        const idNumber = 16378;
+        console.log(userName);
+    }
+   // console.log(idNumber);
+    two();
+}
+one();
